129ecbf30f322b9ea342d43c01bf349f08374be2
justinegrng <jugarnung@gmail.com>
2023-11-21 23:01:23 +0100

    .

--- functions.py
+++ functions.py
@@ -1,4 +1,9 @@
 import os
+
+def print_list(list):
+    for element in list:
+        print(element)
+
 def list_of_files(directory, extension):
     files_names = []
     for filename in os.listdir(directory):
@@ -6,8 +11,44 @@ def list_of_files(directory, extension):
             files_names.append(filename)
     return files_names
 
-def extractname():
-def associerprésident (president):
+def extractname(file_name):
+    return (file_name.split("_")[1]).split(".txt")[0]
+
+def get_all_president_name(files_names):
+    list = []
+    for presidents in files_names:
+        list.append(extractname(presidents))
+    for elem in list:
+        try:
+            if int(elem[:-1]) != 1:
+                list.remove(elem)
+                continue
+            elem.erase[:-1]
+            print(elem)
+        except:
+            print(elem)
+            continue
+
+def clean_speeches(files_names):
+    for file in files_names:
+        f = open("./speeches/" + file, "r", encoding="utf-8")
+        lines = f.readlines()
+        f.close()
+        lines = [line.lower() for line in lines]
+        if not os.path.exists("./cleaned"):
+            os.makedirs("./cleaned")
+        f = open("./cleaned/" + file, "w", encoding="utf-8")
+        for line in lines:
+            if line != "\n":
+                for char in line:
+                    if char == '.' or char == ',' or char == '!' or char == '?' or char == ';' or char == ':':
+                        line = line.replace(char, "")
+                    elif char == '-' or char == "'":
+                        line = line.replace(char, " ")
+                f.write(line)
+        f.close()
+
+def associerprésident(president):
     if president == "Giscard d'Estaing":
         print("Valéry")
     if president == "Macron":
@@ -20,4 +61,5 @@ def associerprésident (president):
         print("Jacques")
     if president == "Mitterand":
         print("François")
+
 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from functions import *
-directory = "./speeches"
-files_names = list_of_files(directory, "txt")
-print_list(files_names)
 
+if __name__ == "__main__":
+    directory = "./speeches"
+    files_names = list_of_files(directory, "txt")
+    print_list(files_names)
+    clean_speeches(files_names)
