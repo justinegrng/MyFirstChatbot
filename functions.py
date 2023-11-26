baaabdd959084b4bc77c1d872f417e1dfd2a27c3
justinegrng <jugarnung@gmail.com>
2023-11-26 19:56:53 +0100

    Modifications du 26/11/2023 19:56

--- functions.py
+++ functions.py
@@ -2,7 +2,6 @@ import os
 import math
 
 # Fonctions de base
-
 def print_list(list):
     for element in list:
         print(element)
@@ -26,11 +25,8 @@ def get_all_president_name(files_names):
         list.append(extract_name(presidents))
     for elem in list:
         try:
-            if int(elem[:-1]) != 1:
-                list.remove(elem)
-                continue
-            elem.pop()
-            print(elem)
+            if int(elem[-1]) ==1:
+                print(elem[:-1])
         except:
             print(elem)
             continue
@@ -170,7 +166,7 @@ def ecologie_word(tf_idf_dict):
     talked_about = {}
     for file in tf_idf_dict:
         for word in tf_idf_dict[file]:
-            if word == "climat" or word == "écologie":
+            if "climat" in word or "écologie" in word:
                 print(extract_name(file))
                 return
 
@@ -182,3 +178,47 @@ def common_words(tf_idf_dict):
         for word in tf_idf_dict[file]:
             if tf_idf_dict[file][word] > 0.5 and tf_idf_dict[file][word] < 1.0:
                 print(word + " : " + str(tf_idf_dict[file][word]))
+
+def menu():
+    print("1.Utiliser les fonctions de base.\n2.Utiliser les fonctionnalités avec TF-IDF.")
+    user = int(input("Que voulez-vous faire ? Choisis 1 ou 2.\n"))
+
+    while user != 2 and user != 1:
+        user = int(input("Que voulez-vous faire ? Choisis 1 ou 2.\n"))
+
+    if user == 1:
+        print("1.Afficher la liste des présidents\n"
+              "2.Afficher les prénoms des présidents associés à leurs noms\n"
+              )
+        choice = int(input("Que voulez vous faire ?\n"))
+        while choice != 2 and choice != 1:
+            choice = int(input("Que voulez vous faire ?\n"))
+        if choice == 1:
+            get_all_president_name(list_of_files("cleaned", "txt"))
+        if choice == 2:
+            president = input("De quel president veux-tu le prénom ?\n"
+                              "Giscard d'Estaing\nMitterand\nMacron\nHollande\nSarkozy\nChirac\n")
+            associate_president(president)
+
+    elif user == 2:
+        print("1. Afficher la liste des mots les moins importants dans le corpus de documents.\n"
+              "2. Afficher le(s) mot(s) ayant le score TD-IDF le plus élevé\n"
+              "3. Indiquer le(s) mot(s) le(s) plus répété(s) par le président Chirac\n"
+              "4. Indiquer le(s) nom(s) du (des) président(s) qui a (ont) parlé de la « Nation » et celui qui l’a répété le plus de fois\n"
+              "5. Indiquer le premier président à parler du climat et/ou de l’écologie\n"
+              "6. Hormis les mots dits « non importants », quel(s) est(sont) le(s) mot(s) que tous les présidents ont évoqués.")
+        choice = int(input("Que voulez vous faire parmi ces 6 fonctionnalités ?\n"))
+        while choice != 2 and choice != 1 and choice != 3 and choice != 4 and choice != 5 and choice != 6:
+            choice = int(input("Que voulez vous faire ?\n"))
+        if choice == 1:
+            non_important_word(tf_idf("cleaned"))
+        elif choice == 2:
+            important_word(tf_idf("cleaned"))
+        elif choice == 3:
+            chirac_most_repeated(tf_idf("cleaned"))
+        elif choice == 4:
+            nation_word(tf_idf("cleaned"))
+        elif choice == 5:
+            ecologie_word(tf_idf("cleaned"))
+        elif choice == 6:
+            common_words(tf_idf("cleaned"))
